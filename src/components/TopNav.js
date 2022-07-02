d69012455f02587959281df1245e23aabbd8de3c
jayansonwong <jayansonwong@gmail.com>
2022-07-02 20:44:18 +0100

    creating initial homepage

---
+++ src/components/TopNav.js
@@ -0,0 +1,20 @@
+import "../style/TopNav.css";
+function TopNav() {
+  return (
+    <ul class="topnav">
+      <h3>
+        <li class="topmenu">
+          <a>Home</a>
+        </li>
+        <li class="topmenu">
+          <a>Something</a>
+        </li>
+        <li class="topmenu">
+          <a>Another thing</a>
+        </li>
+      </h3>
+    </ul>
+  );
+}
+
+export default TopNav;
